89244fe74acc75c827524ae95ae7f2dffdb5aa4b
VSF <viniciussafra@outlook.com>
2021-04-23 18:57:46 -0300

    O nome ser registrado

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,4 +1,22 @@
 class Users::RegistrationsController < Devise::RegistrationsController
 
+  before_action :configure_permitted_parameters
 
+  def new
+    build_resource({})
+    #resource.build_profile_member  // modificar para algo q tenha o nome
+    respond_with self.resource
   end
+
+  protected
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(
+          :sign_up,
+          keys:[
+              :email, :password, :password_confirmation,
+              :name
+          ]
+      )
+    end
+end
